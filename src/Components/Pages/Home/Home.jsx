b4c7a1baa01c5179696a6d9ce8f3b74009be9a4f
chand-stack <chand.stack@gmail.com>
2024-01-24 23:54:47 +0600

    Chore: appointment banner added

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "../../Shared/Banner/Banner";
 import Future from "../../Shared/Future/Future";
 import Locations from "../../Shared/Locations/Locations";
 import Services from "../../Shared/Services/Services";
+import StartBooking from "../../Shared/StartBooking/StartBooking";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Future />
       <Services />
       <Locations />
+      <StartBooking />
     </div>
   );
 };
---
+++ src/Components/Shared/StartBooking/StartBooking.jsx
@@ -0,0 +1,23 @@
+const StartBooking = () => {
+  return (
+    <div>
+      <div
+        className="h-52 md:h-96"
+        style={{
+          backgroundImage: `url(${bg})`,
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="h-full w-full bg-[#34cceb] bg-opacity-60 flex justify-center items-center">
+          <h1 className="text-3xl md:text-5xl font-bold text-white">
+            Contact Us
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StartBooking;
