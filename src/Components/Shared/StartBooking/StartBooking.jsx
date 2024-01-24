c5661f9a5d4899cf0fbe606bf394c5763ad5941e
chand-stack <chand.stack@gmail.com>
2024-01-25 00:33:52 +0600

    Chore: startbooking added

--- src/Components/Shared/StartBooking/StartBooking.jsx
+++ src/Components/Shared/StartBooking/StartBooking.jsx
@@ -1,3 +1,4 @@
+import bg from "../../../assets/upto.png";
 const StartBooking = () => {
   return (
     <div>
@@ -5,15 +6,25 @@ const StartBooking = () => {
         className="h-52 md:h-96"
         style={{
           backgroundImage: `url(${bg})`,
-          backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
         }}
       >
-        <div className="h-full w-full bg-[#34cceb] bg-opacity-60 flex justify-center items-center">
-          <h1 className="text-3xl md:text-5xl font-bold text-white">
-            Contact Us
-          </h1>
+        <div className="h-full w-full bg-gradient-to-r from-black flex justify-center items-center">
+          <div className="container mx-auto space-y-5 md:space-y-9 px-5">
+            <h1 className="text-3xl md:text-5xl font-bold text-white">
+              Bespoke spaces
+            </h1>
+            <p className="text-white md:font-medium md:text-lg ">
+              Expertly designed to create extraordinary spaces with the flexible
+              renter in mind
+            </p>
+            <div className="">
+              <button className="btn text-white bg-[#064749] rounded-full hover:text-black border-none">
+                Start booking
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
