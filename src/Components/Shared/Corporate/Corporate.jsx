ac52073e1ec30dc98a1af424c90a9ce082f16483
chand-stack <chand.stack@gmail.com>
2024-01-26 23:49:52 +0600

    Chore: corporate card added

--- src/Components/Shared/Corporate/Corporate.jsx
+++ src/Components/Shared/Corporate/Corporate.jsx
@@ -1,3 +1,6 @@
+import { CiSearch } from "react-icons/ci";
+import { HiUser } from "react-icons/hi2";
+import { FaFileAlt } from "react-icons/fa";
 const Corporate = () => {
   return (
     <div>
@@ -10,6 +13,23 @@ const Corporate = () => {
           Offer a dedicated booking manager that can help to find properties for
           your needs
         </p>
+      </div>
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-5">
+        <div className="bg-[#E2F1E8] text-black p-10 rounded-2xl">
+          <CiSearch className="text-6xl" />
+          <h1 className="text-2xl font-bold">Booking manager</h1>
+          <p>We do the searching for you</p>
+        </div>
+        <div className="bg-[#E2F1E8] text-black p-10 rounded-2xl">
+          <HiUser className="text-6xl" />
+          <h1 className="text-2xl font-bold">Account manager</h1>
+          <p>Preferred partner rates</p>
+        </div>
+        <div className="bg-[#E2F1E8] text-black p-10 rounded-2xl">
+          <FaFileAlt className="text-6xl" />
+          <h1 className="text-2xl font-bold">Flexible terms</h1>
+          <p>Extend on short notice</p>
+        </div>
       </div>
     </div>
   );
