65ee31946336489cae6ab17212d7f5e985d6c40a
chand-stack <chand.stack@gmail.com>
2024-01-28 17:50:49 +0600

    Chore: readblog section added

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from "../../Shared/Banner/Banner";
 import Corporate from "../../Shared/Corporate/Corporate";
 import Future from "../../Shared/Future/Future";
 import Locations from "../../Shared/Locations/Locations";
+import ReadBlog from "../../Shared/ReadBlog/ReadBlog";
 import Review from "../../Shared/Review/Review";
 import Services from "../../Shared/Services/Services";
 import StartBooking from "../../Shared/StartBooking/StartBooking";
@@ -17,6 +18,7 @@ const Home = () => {
       <StartBooking />
       <Corporate />
       <Review />
+      <ReadBlog />
       <Addbanner />
     </div>
   );
---
+++ src/Components/Shared/ReadBlog/ReadBlog.jsx
@@ -0,0 +1,75 @@
+import { MdWatchLater } from "react-icons/md";
+const ReadBlog = () => {
+  return (
+    <div className="container mx-auto text-black my-24">
+      <h1 className="text-center text-3xl md:text-5xl font-bold">
+        Read our blog
+      </h1>
+      <div className="carousel carousel-center w-full p-4 space-x-4 bg-white rounded-box my-10">
+        <div className="carousel-item">
+          <div className="card w-96 rounded-box bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="text-4xl font-bold">
+                Turpis elit in dictum eget eget
+              </h2>
+              <p>Convallis eu vel fames feugiat et venenatis nulla ipsum.</p>
+              <div className="flex items-center gap-3">
+                <MdWatchLater className="text-2xl" />
+                <p className="text-2xl font-bold">1 min read</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
+            className="rounded-box"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
+            className="rounded-box"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
+            className="rounded-box"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
+            className="rounded-box"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
+            className="rounded-box"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
+            className="rounded-box"
+          />
+        </div>
+      </div>
+      <div className="text-center mt-10">
+        <button className="btn text-white bg-[#064749] rounded-full hover:text-black border-none">
+          Read More
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ReadBlog;
