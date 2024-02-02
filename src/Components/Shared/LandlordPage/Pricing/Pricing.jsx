93bcbed4338850be10f981e0b60f6358470586ca
chand-stack <chand.stack@gmail.com>
2024-02-02 23:55:50 +0600

    Chore: pricing services added

--- src/Components/Shared/LandlordPage/Pricing/Pricing.jsx
+++ src/Components/Shared/LandlordPage/Pricing/Pricing.jsx
@@ -1,3 +1,4 @@
+import { TiTick } from "react-icons/ti";
 const Pricing = () => {
   return (
     <div>
@@ -8,7 +9,40 @@ const Pricing = () => {
         </p>
       </div>
       <div>
-        <div>Monthly Plan</div>
+        <div>
+          <div className="bg-[#E2F1E8] text-center text-black py-10 px-20">
+            <h1 className="text-3xl md:text-5xl font-bold">
+              High Street Agents
+            </h1>
+            <p className="font-semibold">12%</p>
+            <p className="text-xl font-medium">Management fee</p>
+          </div>
+          <div>
+            <p>
+              <TiTick />6 to 12 months
+            </p>
+            <p>
+              <TiTick />
+              No maintenance
+            </p>
+            <p>
+              <TiTick />
+              No weekly cleaning
+            </p>
+            <p>
+              <TiTick />
+              No interior design
+            </p>
+            <p>
+              <TiTick />
+              5% void
+            </p>
+            <p>
+              <TiTick />
+              £24,900
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
